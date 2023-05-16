bce96de781b84484aa3587e0fb9b9753a7147ce9
HitchedLime <sakasa458@gmail.com>
2023-05-16 17:06:37 +0200

    infor from logical camera and yolo

--- scripts/listeners/logic_camera_listener.py
+++ scripts/listeners/logic_camera_listener.py
@@ -1,13 +1,26 @@
 import rospy
-from locobot_simulation.msg import BoundingBoxes
+from locobot_simulation.msg import BoundingBoxes,LogicalImage
 
-def callback(data):
+from geometry_msgs.msg import Twist 
+import message_filters
+rospy.init_node('listener_bounding_box', anonymous=True)
+def callback(data1,data2):
     print("Received BoundingBoxes message:")
-    for box in data.bounding_boxes:
+   # print(data2.pose.position)
+    print(data2.models[0])
+    for box in data1.bounding_boxes:
         print(f"  Class: {box.Class}")
         print(f"  Probability: {box.probability}")
         print(f"  Coordinates: ({box.xmin}, {box.ymin}) - ({box.xmax}, {box.ymax})")
 
-rospy.init_node('listener_bounding_box', anonymous=True)
-rospy.Subscriber('/yolov5/detections', BoundingBoxes, callback)
+
+
+image_sub = message_filters.Subscriber('/gazebo/locobot/camera/logical_camera_image', LogicalImage)
+info_sub = message_filters.Subscriber('/yolov5/detections', BoundingBoxes)
+ts = message_filters.TimeSynchronizer([info_sub,image_sub ], 10)
+ts.registerCallback(callback)
+
+
+#rospy.Subscriber('/yolov5/detections', BoundingBoxes, callback)
 rospy.spin()
+
