74b33799450ed317dae2e744c9c4ec570ef2e886
HitchedLime <sakasa458@gmail.com>
2023-05-16 18:19:12 +0200

    added filter for real and logic camera

--- scripts/listeners/logic_camera_listener.py
+++ scripts/listeners/logic_camera_listener.py
@@ -1,18 +1,43 @@
 import rospy
-from locobot_simulation.msg import BoundingBoxes,LogicalImage
-
-from geometry_msgs.msg import Twist 
+from locobot_simulation.msg import BoundingBoxes,LogicalImage 
 import message_filters
+
+import math
+import re 
 rospy.init_node('listener_bounding_box', anonymous=True)
+
+def euclidean_distance(array):
+    # calculates distance from robot assuming that robot is  at origin every step
+    distance = 0
+    for value in array:
+        distance +=pow(value,2)
+        
+    return math.sqrt(distance)
+
 def callback(data1,data2):
     print("Received BoundingBoxes message:")
-   # print(data2.pose.position)
-    print(data2.models[0])
+    real_camera_bbox=[]
     for box in data1.bounding_boxes:
         print(f"  Class: {box.Class}")
         print(f"  Probability: {box.probability}")
         print(f"  Coordinates: ({box.xmin}, {box.ymin}) - ({box.xmax}, {box.ymax})")
+        real_camera_bbox.append(box.Class)
+        print("Real cam")
 
+    
+    for box in data2.models:
+       #removes numbers  from string 
+        class_name= re.sub(r'[0-9]+', '', box.type)
+       
+        if(class_name.lower()  in real_camera_bbox):
+            print("Fake Cam")
+            print(f"  Class: {box.type}")
+            print(f"  Coordinate x: ({box.pose.position.x})")
+            print(f"  Coordinate y: ({box.pose.position.y})")
+            print(f"  Coordinate z: ({box.pose.position.z})")
+            distance = euclidean_distance([box.pose.position.x,box.pose.position.y,box.pose.position.z])
+            print(f" Distance : ({distance})")
+   
 
 
 image_sub = message_filters.Subscriber('/gazebo/locobot/camera/logical_camera_image', LogicalImage)
