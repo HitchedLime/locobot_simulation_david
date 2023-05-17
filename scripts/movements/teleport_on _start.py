2a1f84149fa201dc97006044006b83c14f11ff7d
HitchedLime <sakasa458@gmail.com>
2023-05-17 18:42:36 +0200

    adde option for different models

--- scripts/movements/teleport_on _start.py
+++ scripts/movements/teleport_on _start.py
@@ -4,14 +4,14 @@ from gazebo_msgs.msg import ModelState
 import sys
 
 
-def  teleport_location(x,y,z):
+def  teleport_location(x,y,z,model="locobot"):
     
     rospy.init_node('set_model_state')
     rospy.wait_for_service('/gazebo/set_model_state')
     set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
     model_state = ModelState()
-    model_state.model_name = 'locobot'
+    model_state.model_name = model
     model_state.pose.position.x = x
     model_state.pose.position.y = y
     model_state.pose.position.z = z
