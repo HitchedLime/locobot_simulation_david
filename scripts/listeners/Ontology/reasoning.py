6ad6876b9424d5acbff6c1d973d77067ac103659
HitchedLime <sakasa458@gmail.com>
2023-05-25 14:30:15 +0200

    reasoner works , rules works

--- scripts/listeners/Ontology/reasoning.py
+++ scripts/listeners/Ontology/reasoning.py
@@ -26,33 +26,33 @@ rdfs = onto.get_namespace("http://www.w3.org/2000/01/rdf-schema")
 xsd = onto.get_namespace("http://www.w3.org/2001/XMLSchema")
 
 
+with onto:
 
-for cls in onto.classes():
-    print(cls.name)
 # Define your SWRL rule using the owlready2 Imp class
-rule = Imp(namespace=ex)
-rule.set_as_rule("hasPoint(?individual1, ?point), hasPoint(?individual2, ?point), differentFrom(?individual1, ?individual2) -> sameAs(?individual1, ?individual2)")
-
-#results = onto.search(type=ex.Chair)
+    rule = Imp(namespace=ex)
+    rule.set_as_rule("hasPoint(?individual1, ?point), hasPoint(?individual2, ?point), differentFrom(?individual1, ?individual2) -> sameAs(?individual1, ?individual2)")
 
 
 
-#results = onto.search(iri="*hasIdentifier")
-
-# Print the results
-# print(results)
-# Add the rule to the ontology
-#onto.add(rule)
-
-# Perform reasoning using the HermiT reasoner
-sync_reasoner()
-
-# Query the ontology to see the inferred triples
 
 
-# Iterate over the results and print them
-# for result in results:
-#     print(result)
 
-# Save the inferred ontology to a file
+    # Perform reasoning using the  reasoner
+    sync_reasoner(infer_property_values = True)
+
+    
+    query = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+SELECT ?individual
+WHERE {
+    ?individual rdf:type/rdfs:subClassOf* <http://example.org/Chair> .
+}
+"""
+    x =list(default_world.sparql(query))
+    print(x)
+
 onto.save(file="changed", format="rdfxml")
